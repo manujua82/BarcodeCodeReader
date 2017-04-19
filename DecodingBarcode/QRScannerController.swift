56517dac07f5d892e9adcf5354a37864d7e43f62
manujua82 <manujua@gmail.com>
2017-04-19 15:37:29 -0400

    Create Interface

--- DecodingBarcode/QRScannerController.swift
+++ DecodingBarcode/QRScannerController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  QRScannerController.swift
 //  DecodingBarcode
 //
 //  Created by Kevin Bilberry on 4/5/17.
@@ -8,18 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class QRScannerController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
-
+   
 }
-
